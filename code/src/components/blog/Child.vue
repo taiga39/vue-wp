b22237ecf597b0fbd93be801773ce935bc23ead1
taiga39 <ishikawa.tigers@gmail.com>
2020-07-20 00:36:29 +0900

    投稿について

---
+++ code/src/components/blog/Child.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="col-lg-8 col-md-10 mx-auto">
+        <div v-for="json in jsondata.blog" :key="json.link" class="post-preview">
+            <router-link v-bind:to="'/blog/' + json.link">
+                <h2 class="post-title">
+                    {{json.title}}
+                </h2>
+            </router-link>
+            <p class="post-meta" style="padding-left:1em">
+                Posted 
+                on {{json.date}}  
+            </p>
+        </div>
+        <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jsonlength: 0
+        }
+    },
+    props:["jsondata"],
+    mounted() {
+        if(this.jsondata.blog != null){
+            this.jsonlength = JSON.parse(JSON.stringify(Object.keys(this.jsondata.blog).length))
+        }
+    },
+    watch: {
+        jsondata:function(one){
+            this.jsonlength = JSON.parse(JSON.stringify(Object.keys(one.blog).length))
+        }
+    }
+}
+</script>
---
+++ code/src/components/tech/Child.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="col-lg-8 col-md-10 mx-auto">
+        <div v-for="json in jsondata.tech" :key="json.link" class="post-preview">
+            <router-link v-bind:to="'/tech/' + json.link">
+                <h2 class="post-title">
+                    {{json.title}}
+                </h2>
+            </router-link>
+            <p class="post-meta" style="padding-left:1em">
+                Posted 
+                on {{json.date}}  
+            </p>
+        </div>
+        <div class="clearfix">
+            <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jsonlength: 0
+        }
+    },
+    props:["jsondata"],
+    mounted() {
+        if(this.jsondata.tech != null){
+            this.jsonlength = JSON.parse(JSON.stringify(Object.keys(this.jsondata.tech).length))
+        }
+    },
+    watch: {
+        jsondata:function(one){
+            this.jsonlength = JSON.parse(JSON.stringify(Object.keys(one.tech).length))
+        }
+    }
+}
+</script>
